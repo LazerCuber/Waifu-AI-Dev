72171f681ac366fdc781ddb2f0bd77db2d3d86a1
LazerCuber <haydeniscuber@gmail.com>
2024-10-05 23:02:47 +0800

    what did i cook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import dynamic from 'next/dynamic';
 import ChatInput from "~/components/ChatInput";
 import React from 'react';
@@ -29,18 +29,22 @@ export default function Page() {
     };
   }, []);
 
+  const renderContents = useMemo(() => {
+    return live2dLoaded && (
+      <>
+        <ChatterBox />
+        <Model />
+      </>
+    );
+  }, [live2dLoaded]);
+
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center overflow-hidden">
       <Background />
       <div className="relative z-10 flex flex-col items-center justify-center w-full h-full">
         <ChatInput />
         <div className="h-screen flex justify-center items-center w-full">
-          {live2dLoaded && (
-            <>
-              <ChatterBox />
-              <Model />
-            </>
-          )}
+          {renderContents}
         </div>
       </div>
     </main>
