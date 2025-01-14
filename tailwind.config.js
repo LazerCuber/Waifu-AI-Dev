fbfe49eac7426daa722047e25fe598c09b9f3a84
LazerTC <haydeniscuber@gmail.com>
2025-01-14 11:09:41 +0800

    revert

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
