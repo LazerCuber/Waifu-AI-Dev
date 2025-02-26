b29af497b962c914d72ef79be253bf4347d54d3b
CreonC <creon080@gmail.com>
2025-02-26 22:11:30 +0800

    chore: adjust TypeScript strictness settings and add a blank line in App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import { Model } from '../components/Model';
 import { ChatInput } from '../components/ChatInput';
 import { GeistSans } from "geist/font/sans";
 
+
 export function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
